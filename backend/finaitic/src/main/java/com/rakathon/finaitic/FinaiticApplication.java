3a327d8f9077849c39c19bbb6092132cc662bf02
yash.kesharwani <yash.kesharwani@rakuten.com>
2023-09-08 11:03:53 +0530

    initialize

---
+++ backend/finaitic/src/main/java/com/rakathon/finaitic/FinaiticApplication.java
@@ -0,0 +1,13 @@
+package com.rakathon.finaitic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaiticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaiticApplication.class, args);
+	}
+
+}
---
+++ backend/finaitic/src/test/java/com/rakathon/finaitic/FinaiticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakathon.finaitic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaiticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
